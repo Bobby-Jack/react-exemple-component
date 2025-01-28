6fc85d67d85dee01b631d6bfd343101675a76ab3
Bobby-Jack <pigpongproduction@gmail.com>
2025-01-28 16:25:59 +0100

    ru

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,19 @@ function App() {
   // ces données seront fournit au component MyCard pour le paramètre content
   const dataCard = {
     name : "ryad",
-    age : 24
+    age : 55
   }
+  const m = "light"
   
   return (
     <>
     
       
       {/* ici, on fourni à content une constent */}
-      <MyCard content={dataCard}/> 
+      <MyCard content={dataCard} m={m}/> 
       {/* ici on fourni une valeur brut à content */}
-      <MyCard content={{name:"Pawl", age: 12}}/>
+      <MyCard content={{name:"Pawl", age: 12}} m={m}/>
+      <MyCard content={{name:"Pawl", age: 42}} m={m}/>
 
       
     </>
--- src/components/MyCard/MyCard.jsx
+++ src/components/MyCard/MyCard.jsx
@@ -4,15 +4,29 @@ import { FaBeer } from "react-icons/fa";
 
 // le component est un fonction qui prend en paramètre des valeurs servant à son affichage.
 // ces valeur sont fournient par le parent (App.jsx)
-function MyCard({content}){
+function MyCard({content, m}){
   
       // le return fourni le résultat html à charger dans la page index
     return (
-          <div className="MyCard">
+          
+      //      cette condition ternaire vérifie si le mode sombre est activé et applique une classe suplementaire en conséquence
+          <div className={m=="dark" ? "MyCard dark" : "MyCard"}>
             
                 {content.name} age : {content.age} 
+                
                 <FaBeer/>
+                <hr />
+                {/* autre affichage conditionelle */}
+                {
+                  content.age >= 50 ?
+                  <>
+                        <h3>OLD!</h3>
+                  </>
+                  :
+                  ""
+                }
           </div>
+          
     
     )
 }
